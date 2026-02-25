c98c564d43c40bce77c76c0cc9175b892b00a5da
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2026-02-25 16:45:29 +0100

    Added OrderItemRepository interface

---
+++ src/main/java/se/lexicon/ecommerceplatform/repository/OrderItemRepository.java
@@ -0,0 +1,16 @@
+package se.lexicon.ecommerceplatform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.lexicon.ecommerceplatform.entity.OrderItem;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    Optional<OrderItem> findByOrder_Id(Long orderId);
+
+    List<OrderItem> findByProduct_Id(Long productId);
+
+    List<OrderItem> findByQuantityGreaterThan(Integer quantity);
+}
