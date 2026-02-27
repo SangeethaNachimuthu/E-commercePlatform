29cbaf2fe9d82534e8e6cffc42e90377d97553e2
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2026-02-27 16:52:55 +0100

    Added native query for OrderRepository

--- src/main/java/se/lexicon/ecommerceplatform/repository/OrderRepository.java
+++ src/main/java/se/lexicon/ecommerceplatform/repository/OrderRepository.java
@@ -2,14 +2,18 @@ package se.lexicon.ecommerceplatform.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.ecommerceplatform.entity.Order;
 import se.lexicon.ecommerceplatform.entity.OrderStatus;
 
 import java.time.Instant;
 import java.util.List;
 
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    //SELECT * FROM orders WHERE customer_id = ?;
     List<Order> findByCustomer_Id(Long customerId);
 
     /* The EntityGroup annotation will fetch Orders and their items in one query using a join internally.
@@ -20,13 +24,21 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @EntityGraph(attributePaths = "items")
     List<Order> findByStatus(OrderStatus status);
 
+    //SELECT * FROM orders WHERE order_date > ?;
     List<Order> findByOrderDateAfter(Instant orderDateAfter);
 
+    //SELECT * FROM orders WHERE order_date BETWEEN ? AND ?;
     List<Order> findByOrderDateBetween(Instant orderDateAfter, Instant orderDateBefore);
 
     //Find orders that contain a specific product.
+    //SELECT DISTINCT o.* FROM orders o
+    //        JOIN order_items i ON o.id = i.order_id WHERE i.product_id = ?;
+    @Query("SELECT DISTINCT o FROM Order o JOIN o.items i WHERE i.product.Id = ?")
+    List<Order> findByProductId(@Param("product_id") Long product_id);
 
+    //SELECT COUNT(*) FROM orders WHERE status = ?;
     long countByStatus(OrderStatus status);
 
+    //SELECT * FROM orders WHERE customer_id = ? AND status = ?;
     List<Order> findByCustomer_IdAndStatus(Long customerId, OrderStatus status);
 }
