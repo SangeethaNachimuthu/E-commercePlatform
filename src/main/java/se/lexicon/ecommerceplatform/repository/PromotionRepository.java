ced7712509b1a915d7c16647c9ebc6d3a45766b9
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2026-02-27 17:13:47 +0100

    Added native query for PromotionRepository

--- src/main/java/se/lexicon/ecommerceplatform/repository/PromotionRepository.java
+++ src/main/java/se/lexicon/ecommerceplatform/repository/PromotionRepository.java
@@ -8,16 +8,22 @@ import java.util.List;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
 
+    //SELECT * FROM promotions WHERE start_date <= ? AND (end_date IS NULL OR end_date >= ?);
     List<Promotion> findByStartDateAfterAndEndDateBefore(LocalDate startDateAfter, LocalDate endDateBefore);
 
+    //SELECT * FROM promotions WHERE code = ?;
     List<Promotion> findByCode(String code);
 
+    //SELECT * FROM promotions WHERE start_date > ?;
     List<Promotion> findByStartDateAfter(LocalDate startDateAfter);
 
+    //SELECT * FROM promotions WHERE end_date < ?;
     List<Promotion> findByEndDateBefore(LocalDate endDateBefore);
 
+    //SELECT * FROM promotions WHERE end_date IS NULL;
     List<Promotion> findByEndDateIsNull(LocalDate endDate);
 
+    //SELECT * FROM promotions WHERE start_date <= CURRENT_DATE AND (end_date >= CURRENT_DATE OR end_date IS NULL);
     List<Promotion> findByStartDateLessThanEqualAndEndDateGreaterThanEqual(
             LocalDate startDateIsLessThan, LocalDate endDateIsGreaterThan);
 }
