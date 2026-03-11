54a9ae60ac6fb92d9f09138d1baafb04a9d9ca5e
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2026-03-11 13:03:43 +0100

    Adding Task 1 OrderController

---
+++ src/main/java/se/lexicon/ecommerceplatform/controller/OrderController.java
@@ -0,0 +1,30 @@
+package se.lexicon.ecommerceplatform.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.ecommerceplatform.dto.request.OrderRequestDTO;
+import se.lexicon.ecommerceplatform.dto.response.OrderResponseDTO;
+import se.lexicon.ecommerceplatform.service.OrderService;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDTO> create(@Valid @RequestBody OrderRequestDTO requestDTO) {
+
+        OrderResponseDTO responseDTO = orderService.placeOrder(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+}
